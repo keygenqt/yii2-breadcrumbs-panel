d434fe8c9e66d6ddc0732fd58c75352a4db5ed4d
KeyGenQt <keygenqt@gmail.com>
2016-01-25 13:31:22 +0300

    fix

--- FontAwesomeAssets.php
+++ FontAwesomeAssets.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace keygenqt\verticalMenu;
+namespace keygenqt\breadcrumbsPanel;
 
 use \yii\web\AssetBundle;
 
--- views/view.php
+++ views/view.php
@@ -1,31 +1,57 @@
 <?php
     /* @var $widget keygenqt\breadcrumbsPanel\BreadcrumbsPanel */
+
+    use \yii\widgets\Breadcrumbs;
 ?>
 
 <style>
     .yii2-breadcrumbs-panel {
-        
+        width: 100%;
+        display: table;
     }
     .yii2-breadcrumbs-panel .breadcrumbs-panel {
-        
+        background: #3d0047;
+        display: table-row;
     }
     .yii2-breadcrumbs-panel .content-panel {
-        
+        border: 1px solid #3d0047;
+        display: table-row;
     }
+    .yii2-breadcrumbs-panel ul {
+        margin: 0;
+        padding: 10px 20px;
+    }
+    .yii2-breadcrumbs-panel ul li {
+        display: inline-block;
+        color: white;
+    }
+    .yii2-breadcrumbs-panel ul li a {
+        color: white;
+    }
+    .yii2-breadcrumbs-panel ul li:after {
+        content: "/";
+        margin-left: 10px;
+        margin-right: 3px;
+    }
+    .yii2-breadcrumbs-panel ul li:last-child:after {
+        content: "";
+    }
+    
+    
+    
+
 </style>
 
 <div class="yii2-breadcrumbs-panel">
     <div class="breadcrumbs-panel">
         <?= Breadcrumbs::widget([
-            'itemTemplate' => "<li><i>{link}</i></li>\n", // template for all links
+            'options' => [],
             'links' => [
                 [
-                    'label' => 'Post Category',
-                    'url' => ['post-category/view', 'id' => 10],
-                    'template' => "<li><b>{link}</b></li>\n", // template for this link only
+                    'label' => 'Sample Post', 
+                    'url' => ['post/edit', 'id' => 1]
                 ],
-                ['label' => 'Sample Post', 'url' => ['post/edit', 'id' => 1]],
-                'Edit',
+                'Edit'
             ],
         ]) ?>
     </div>
