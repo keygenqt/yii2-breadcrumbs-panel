9b1cc5cb94aafb962a8871b0f03729e2e4cf99cc
KeyGenQt <keygenqt@gmail.com>
2016-01-25 14:16:44 +0300

    fix

--- BreadcrumbsPanel.php
+++ BreadcrumbsPanel.php
@@ -4,13 +4,24 @@ namespace keygenqt\breadcrumbsPanel;
 
 class BreadcrumbsPanel extends \yii\widgets\Menu
 {
-    public $icon;
+    public static $links = [];
+    public static $icon = '';
+    
+    public $homeLink = true;
     public $content;
+    
+    public static $_init = false;
     
     public function run()
 	{
         FontAwesomeAssets::register($this->getView());
         ActiveAssets::register($this->getView());
 		return  $this->getView()->render('@keygenqt/breadcrumbsPanel/views/view', ['widget' => $this]);
+    }
+    
+    public static function setParams($links, $icon = '')
+    {
+        self::$links = $links;
+        self::$icon = $icon;
     }
 }
--- views/view.php
+++ views/view.php
@@ -2,60 +2,38 @@
     /* @var $widget keygenqt\breadcrumbsPanel\BreadcrumbsPanel */
 
     use \yii\widgets\Breadcrumbs;
+    use keygenqt\breadcrumbsPanel\BreadcrumbsPanel;
 ?>
 
-<style>
-    .yii2-breadcrumbs-panel {
-        width: 100%;
-        display: table;
-    }
-    .yii2-breadcrumbs-panel .breadcrumbs-panel {
-        background: #3d0047;
-        display: table-row;
-    }
-    .yii2-breadcrumbs-panel .content-panel {
-        border: 1px solid #3d0047;
-        display: table-row;
-    }
-    .yii2-breadcrumbs-panel ul {
-        margin: 0;
-        padding: 10px 20px;
-    }
-    .yii2-breadcrumbs-panel ul li {
-        display: inline-block;
-        color: white;
-    }
-    .yii2-breadcrumbs-panel ul li a {
-        color: white;
-    }
-    .yii2-breadcrumbs-panel ul li:after {
-        content: "/";
-        margin-left: 10px;
-        margin-right: 3px;
-    }
-    .yii2-breadcrumbs-panel ul li:last-child:after {
-        content: "";
-    }
-    
-    
-    
-
-</style>
+<?php if ($widget->homeLink === false): ?>
+    <style>
+        .yii2-breadcrumbs-panel ul li:first-child {
+            display: none;
+        }
+    </style>
+<?php endif; ?>
 
 <div class="yii2-breadcrumbs-panel">
     <div class="breadcrumbs-panel">
-        <?= Breadcrumbs::widget([
-            'options' => [],
-            'links' => [
-                [
-                    'label' => 'Sample Post', 
-                    'url' => ['post/edit', 'id' => 1]
-                ],
-                'Edit'
-            ],
-        ]) ?>
+        <div class="breadcrumbs-cell">
+            
+            <?php if (BreadcrumbsPanel::$icon): ?>
+                <div class="breadcrumbs-icon">
+                    <i class="<?= BreadcrumbsPanel::$icon ?>"></i>
+                </div>
+            <?php endif; ?>
+            
+            <?php if (BreadcrumbsPanel::$links): ?>
+                <?= Breadcrumbs::widget([
+                    'options' => [],
+                    'links' => BreadcrumbsPanel::$links
+                ]) ?>
+            <?php endif; ?>
+        </div>
     </div>
     <div class="content-panel">
-        <?= $widget->content ?>
+        <div class="breadcrumbs-cell">
+            <?= $widget->content ?>
+        </div>
     </div>
 </div>
